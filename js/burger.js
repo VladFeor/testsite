cb4b754959e4d569fb6c8af8066c244c37f30185
VladFeor <98813579+VladFeor@users.noreply.github.com>
2022-04-25 19:50:52 +0300

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,7 +1,20 @@
 const burger  = document.querySelectorAll('.burger__nav')
 const burgermenu  = document.querySelector('.burger__menu')
 const body  = document.querySelector('body')
+const main  = document.querySelector('.main')
 
+
+console.log('connect!')
+
+main.addEventListener('click',()=>{
+    if(!burgermenu.classList.contains('normal')){
+     
+    }
+    else{
+        burgermenu.classList.remove('normal')
+        body.classList.remove('noscroll')
+    }
+})
 burger.forEach(item =>{
     item.addEventListener('click',()=>{
     if(!burgermenu.classList.contains('normal')){
